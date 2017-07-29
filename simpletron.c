cfcd014770a3352a6bf1d2e1f33b669ad42bd275
CProgrammerLiang <30545936+CProgrammerLiang@users.noreply.github.com>
2017-07-29 15:40:30 +0800

    Add files via upload
    to be continued
    this program is based on c:how to program c,copyright h m detiel p j detiel
    already translated in chinese second edition

---
+++ simpletron.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define READ 10
+#define WRITE 11
+#define LOAD 20
+#define STORE 21
+#define ADD 30
+#define SUBSTRACT 31
+#define DEVIDE 32
+#define MULTIPLY 33
+#define BRANCH 40
+#define BRANCHNEG 41
+#define BRANCHZERO 42
+#define HALT 43
+main(){
+	int instructionCounter,operationCode,operand;
+	signed accumulator,instructionRegister;
+	instructionCounter=0;
+	instructionCounter=0;
+	instructionCounter=0;
+	instructionCounter=0;
+	instructionCounter=0;
+}
